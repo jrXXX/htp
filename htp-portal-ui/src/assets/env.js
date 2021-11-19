23aad0420a357d40b8c3a3590f594f3a250271cb
Richter Jan <richja@akros.ch>
2021-11-19 13:24:19 +0100

    htp-hotel-vendor-ui

--- htp-portal-ui/src/assets/env.js
+++ htp-portal-ui/src/assets/env.js
@@ -1,8 +1,12 @@
 (function(window) {
     window["env"] = window["env"] || {};
-  
+ 
     // Environment variables
-    window["env"]["backendUrl"] = "http://localhost:8080";
-    window["env"]["ownUrl"] = "http://localhost:4200";
+    window["env"]["backendUrl"] = "http://htp-portal-backend-htp.apps-crc.testing";
+    window["env"]["ownUrl"] = "http://htp-portal-ui-htp.apps-crc.testing";
+
+    // Environment variables
+    // window["env"]["backendUrl"] = "http://localhost:8080";
+    // window["env"]["ownUrl"] = "http://localhost:4200";
   })(this);
   
