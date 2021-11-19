0b309ff497976de229b340fe05be05d2f0cab910
Richter Jan <richja@akros.ch>
2021-11-19 11:43:40 +0100

    htp-portal-ui environment vars fixed

---
+++ htp-portal-ui/src/assets/env.openshift.js
@@ -0,0 +1,8 @@
+(function(window) {
+    window["env"] = window["env"] || {};
+  
+    // Environment variables
+    window["env"]["backendUrl"] = "http://htp-portal-backend-htp.apps-crc.testing";
+    window["env"]["ownUrl"] = "http://htp-portal-ui-htp.apps-crc.testing";
+  })(this);
+  
