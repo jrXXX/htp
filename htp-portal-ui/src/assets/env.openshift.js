a6b2b4dc5f3fdae0dd3c72f8208cff91ca71cd59
Richter Jan <richja@akros.ch>
2021-11-22 10:40:58 +0100

    docker config htp-portal-ui updated

--- htp-portal-ui/src/assets/env.openshift.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["backendUrl"] = "http://htp-portal-backend-htp.apps-crc.testing";
-    window["env"]["ownUrl"] = "http://htp-portal-ui-htp.apps-crc.testing";
-  })(this);
-  
